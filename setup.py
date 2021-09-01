524abe2d09bee7d839a37d0e2f7fe52a75766570
Fernando.Gutierrez <fernando.gutierrez@jalasoft.com>
2021-08-31 20:57:20 -0400

    Task #3
    Created swagger documentation of CRUD operations for drivers and trucks

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="truck_delivery",
+    version="0.0.1",
+    author="fernando",
+    description="an application to manage trucks",
+    requires_dist=['flask']
+)
