f73b30a2d50adc9c238712191b1676c370d6a0df
Fernando.Gutierrez <fernando.gutierrez@jalasoft.com>
2021-09-27 21:19:00 -0400

    - Remove commented code at "main.py" file and Fix "save_client" endpoint

--- trucks_project/api/main.py
+++ trucks_project/api/main.py
@@ -3,8 +3,6 @@ from flask import Flask, request, jsonify, abort
 from api.util.random import generate_random_uuid
 from truck_delivery_fernando.client_manager import ClientManager
 
-# from api.views import client_api
-
 API_NAME = "/api/v1"
 app = Flask(__name__)
 
@@ -14,7 +12,9 @@ def save_client():
     client_params = json.dumps(request.json)
     id = generate_random_uuid()
     result = ClientManager().save_document(id, client_params)
-    return {"status": "Success", "id": id} if result else {"status": "Failed"}
+    if not result:
+        abort(400, "Bad Request, please review the parameters sent.")
+    return {"status": "Success", "id": id}
 
 
 @app.route(f"{API_NAME}/clients", methods=["GET"])
@@ -38,11 +38,6 @@ def delete_client_by_id(client_id):
         abort(404, "Client not found!.")
     return {"status": "Success", "message": f"Client id [ {client_id} ] deleted from DB."}
 
-
-# app.add_url_rule("/clients", view_func=client_api.get_all_clients)
-# app.add_url_rule("/clients", view_func=client_api.save_client, methods=["POST"])
-# app.add_url_rule("/client/<client_id>", view_func=client_api.get_client_by_id)
-# app.add_url_rule("/client/<client_id>", view_func=client_api.delete_client_by_id, methods=["DELETE"])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5000, use_reloader=True)
